46cd29f29f9e695d15ad82ccb6005805c0fd3908
Mohamed Riyaz Ahamed <91612487+Darkknight1213@users.noreply.github.com>
2024-11-27 21:11:40 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+#Program to find the inverse of a matrix.
+#Developed by: Mohamed Riyaz Ahamed
+#RegisterNumber: 24900085 
+
+import numpy as np
+a = np.array([[6,2,3], [3,1,1,], [10,3,4]])
+b= np.linalg.inv(a)
+print(b)
